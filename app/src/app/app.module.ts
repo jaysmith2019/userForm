efc34df3c75de4b51d70771351a5af166055b7b3
jaysmith2019 <jsmith@paradymemanagement.com>
2019-10-09 19:13:21 -0400

    completion of basic form

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule }   from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/src/app/card/card.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Input, Directive } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+
+export class CardComponent implements OnInit {
+  @Input() title;
+  userForm = this.fb.group({
+    Name: [''],
+    Address: [''],
+  });
+  isValid: boolean = false;
+  isSuccess: boolean = false;
+  errorMessage: string = "Address and name are required values.";
+  successMessage: string = "The users information was saved successfully.";
+constructor(private fb: FormBuilder) { }
+ngOnInit() {
+  }
+  save() {
+    if(this.userForm.value.Name === "" && this.userForm.value.Address !== "") {
+      this.errorMessage = "Name is a required field.";
+      this.isValid = false;
+    } else if(this.userForm.value.Address === "" && this.userForm.value.Name !== "") {
+      this.errorMessage = "Address is a required field."
+      this.isValid = false;
+    } else if(this.userForm.value.Name === "" && this.userForm.value.Address === "") {
+      this.errorMessage = "Address and name are required values.";
+      this.isValid = false;
+    }
+    else {
+      this.isValid = true;
+
+    }
+    console.log(this.userForm.value.Name);
+   }
+}
